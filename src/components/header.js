66e72d2c0ed3585f2581b76859d0480cf1a5aa0c
Ethan Ferry <40361386+ethanrferry@users.noreply.github.com>
2018-07-22 17:54:26 -0600

    figured out how to drop stuff in here

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = (title, content) => {
+    return (
+        <div className="header">
+            <h1>Bottega Madlibs</h1>
+            <p>Fill out the field below and click the generate button to see the madlib story</p>
+        </div>
+    )
+}
+
+export default Header;
