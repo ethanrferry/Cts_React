a48c2d1c8d525e50984fd4f1ece81e6c765fdabb
Ethan Ferry <40361386+ethanrferry@users.noreply.github.com>
2018-07-25 09:55:05 -0600

    finished madlibs project

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
-const Header = (title, content) => {
+const Header = () => {
     return (
         <div className="header">
+            <div className="header__skew"></div>
+            <div className="header__border"></div>
             <h1>Bottega Madlibs</h1>
             <p>Fill out the field below and click the generate button to see the madlib story</p>
         </div>
